a2b4d5651b314d1a669ebde51781d643ed61cb63
smajic07 <smajic.edin.7@gmail.com>
2024-05-12 17:38:36 +0200

    Formatted some files spacings

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,8 +10,8 @@ const Card = ({ imageSrc, imageAlt, title }: CardProps) => {
   return (
     <article className={styles['wrapper']}>
       {/* TODO: Add backup placeholder img if imageSrc is falsy */}
-        <img src={imageSrc} alt={imageAlt || title} className={styles['image']} />
-        <h2 className={styles['title']}>{title}</h2>
+      <img src={imageSrc} alt={imageAlt || title} className={styles['image']} />
+      <h2 className={styles['title']}>{title}</h2>
     </article>
   )
 }
